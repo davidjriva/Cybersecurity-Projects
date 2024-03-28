42c5548ed90441961a57334fcc59d365bc0caf60
David Riva <davidjriva@gmail.com>
2024-03-28 14:40:30 -0600

    updates

--- PA1/run_decrypt_tests.sh
+++ PA1/run_decrypt_tests.sh
@@ -2,11 +2,9 @@
 
 for number in {1..3}
 do
-    touch tmp.txt
+    ./cipher B ./Test-Cases/Block-Cipher/"$number"/bc-encrypted-output-tc-"$number".txt ./Text-Files/tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-key-tc-"$number".txt D
 
-    ./cipher B ./Test-Cases/Block-Cipher/"$number"/bc-encrypted-output-tc-"$number".txt tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-key-tc-"$number".txt D
-
-    if diff_output=$(diff tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-decrypted-output-tc-"$number".txt); then
+    if diff_output=$(diff ./Text-Files/tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-decrypted-output-tc-"$number".txt); then
         echo "Decryption test $number: No differences found."
     else
         echo "Decryption test $number: Differences found:"
--- PA1/run_encrypt_tests.sh
+++ PA1/run_encrypt_tests.sh
@@ -2,12 +2,10 @@
 
 for number in {1..3}
 do
-    touch tmp.txt
+    ./cipher B ./Test-Cases/Block-Cipher/"$number"/bc-input-tc-"$number".txt ./Text-Files/tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-key-tc-"$number".txt E
 
-    ./cipher B ./Test-Cases/Block-Cipher/"$number"/bc-input-tc-"$number".txt tmp.txt ./Test-Cases/Block-Cipher/"$number"/bc-key-tc-"$number".txt E
-
-    hexdump -C tmp.txt > tmp2.txt
-    if diff_output=$(diff tmp2.txt ./Test-Cases/Block-Cipher/"$number"/bc-encrypted-output-hexdump-tc-"$number".txt); then
+    hexdump -C ./Text-Files/tmp.txt > ./Text-Files/tmp2.txt
+    if diff_output=$(diff ./Text-Files/tmp2.txt ./Test-Cases/Block-Cipher/"$number"/bc-encrypted-output-hexdump-tc-"$number".txt); then
         echo "Encryption test $number: No differences found."
     else
         echo "Encryption test $number: Differences found:"
